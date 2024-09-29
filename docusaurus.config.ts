847467b7acfa6e164b87e1adc6573d53b2f61f42
Leo Cappiello <54512529+Leocappiello@users.noreply.github.com>
2024-09-29 15:23:21 -0300

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,10 +9,10 @@ const config: Config = {
   favicon: 'img/favicon_io/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://github.com/Leocappiello',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/mega-store-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
